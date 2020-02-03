c96c25c5abc7d9dc0a100664446ce950f8873a5e
atsushi <muraishiatsushiii@gmail.com>
2020-02-03 10:58:27 +0000

    add show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get  '/contact',   to: 'static_pages#contact'
   get  '/campaign',  to: 'static_pages#campaign'
   get  '/signup',    to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
