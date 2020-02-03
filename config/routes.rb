004ace104bb1c3a768270e5e40b758295f2e55e6
atsushi <muraishiatsushiii@gmail.com>
2020-02-03 09:10:36 +0000

    add a basic new page without form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home' # => root_path
   get  '/contact',   to: 'static_pages#contact'
-  get  '/campaign', to: 'static_pages#campaign'
-
+  get  '/campaign',  to: 'static_pages#campaign'
+  get  '/signup',    to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
