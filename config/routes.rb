83827ef98b1e63d49f70bca6e81aee5a60fa5eec
atsushi <muraishiatsushiii@gmail.com>
2020-02-03 08:49:23 +0000

    add campaign page without form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home' # => root_path
   get  '/contact',   to: 'static_pages#contact'
+  get  '/campaign', to: 'static_pages#campaign'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
