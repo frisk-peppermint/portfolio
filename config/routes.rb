ecebe81cefa4d4da02ff36b5d040cdd6a1886be0
atsushi <muraishiatsushiii@gmail.com>
2020-02-03 11:50:27 +0000

    create a basic signup page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get  '/contact',   to: 'static_pages#contact'
   get  '/campaign',  to: 'static_pages#campaign'
   get  '/signup',    to: 'users#new'
+  post '/signup',    to: 'users#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
