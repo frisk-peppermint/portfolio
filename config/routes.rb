bc581c041a005d4252324bcd3e3f692858653d05
atsushi <muraishiatsushiii@gmail.com>
2020-02-03 08:59:04 +0000

    add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home' # => root_path
   get  '/contact',   to: 'static_pages#contact'
   get  '/campaign', to: 'static_pages#campaign'
