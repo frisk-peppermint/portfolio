82aeb48273afcd63a8ca9b79235cd3d61450ec2a
atsushi <muraishiatsushiii@gmail.com>
2020-02-03 07:36:38 +0000

    make a basic contact page without form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home' # => root_path
+  get  '/contact',   to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
