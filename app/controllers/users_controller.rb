ec5a27dab2e3ac57b2c4ed6fce1bbcf14ed3dcc8
atsushi <muraishiatsushiii@gmail.com>
2020-02-04 08:14:33 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ご登録ありがとうございます"
       redirect_to @user
     else
@@ -20,8 +21,7 @@ class UsersController < ApplicationController
    private
 
     def user_params
-      params.require(:user).permit(:name, :tool, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :tool, :password, :password_confirmation)
     end
     
 end
