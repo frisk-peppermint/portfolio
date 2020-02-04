18f6a9d0627ca56fc1622bafb6f9d7b902e5e454
atsushi <muraishiatsushiii@gmail.com>
2020-02-04 01:34:50 +0000

    edit roots.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home' # => root_path
-  get  '/contact',   to: 'static_pages#contact'
-  get  '/campaign',  to: 'static_pages#campaign'
-  get  '/signup',    to: 'users#new'
-  post '/signup',    to: 'users#create'
+  get    '/contact',   to: 'static_pages#contact'
+  get    '/campaign',  to: 'static_pages#campaign'
+  get    '/signup',    to: 'users#new'
+  post   '/signup',    to: 'users#create'
+  
+  get    '/login',     to: 'sessions#new'
+  post   '/login',     to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
