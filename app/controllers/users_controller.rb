28667c19e11046b21f4289dd26dba9708a8244df
atsushi <muraishiatsushiii@gmail.com>
2020-02-04 12:51:46 +0000

    『違うユーザーからのアクセスを保護する機能を実装しました。』

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user,   only: [:edit, :update, :show]
   
   def new
     @user = User.new
@@ -43,6 +43,7 @@ class UsersController < ApplicationController
     
     def logged_in_user
       unless logged_in?
+      store_location
         flash[:danger] = "ログインを行っってください"
         redirect_to login_url
       end
