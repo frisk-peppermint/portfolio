746d0950b35c69f9d403a31452536904ed46cda3
atsushi <muraishiatsushiii@gmail.com>
2020-02-04 11:30:56 +0000

    「追加」　違うユーザーがユーザー情報を編集できないようにしました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -25,6 +28,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      flash[:success] = "アカウント情報が更新されました"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -36,4 +41,15 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :tool, :password, :password_confirmation)
     end
     
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインを行っってください"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
