0cf9fa12a264d1e27e8df667df70ff3c27d5d7e8
atsushi <muraishiatsushiii@gmail.com>
2020-02-05 04:47:11 +0000

    「追加」 staffsテーブルのemailカラムにインデックスを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205041711) do
+ActiveRecord::Schema.define(version: 20200205044227) do
 
   create_table "staffs", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_staffs_on_email", unique: true
   end
 
   create_table "users", force: :cascade do |t|
