3e08141ff76f95555a33a6f66b59e77243a14891
atsushi <muraishiatsushiii@gmail.com>
2020-02-05 04:51:57 +0000

    「追加」 パスワードダイジェストを追加し、マイグレートしました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205044227) do
+ActiveRecord::Schema.define(version: 20200205045028) do
 
   create_table "staffs", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_staffs_on_email", unique: true
   end
 
