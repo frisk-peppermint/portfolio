e222e9b87419cab426ac20264c97b331b06de56f
atsushi <muraishiatsushiii@gmail.com>
2020-02-05 04:19:24 +0000

    「追加」　staffモデルを追加し、マイグレートしました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200203093555) do
+ActiveRecord::Schema.define(version: 20200205041711) do
+
+  create_table "staffs", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
