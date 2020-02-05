f143ed7ba972d7a3206b4da3979516e969a8008f
atsushi <muraishiatsushiii@gmail.com>
2020-02-05 05:44:31 +0000

    「追加」　フォームでエラーを表示させるようにしました。

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,8 +1,25 @@
 class StaffsController < ApplicationController
   def new
+    @staff = Staff.new
   end
   
   def show
     @staff = Staff.find(params[:id])
   end
+  
+  def create
+    @staff = Staff.new(staff_params)    # 実装は終わっていないことに注意!
+    if @staff.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def staff_params
+      params.require(:staff).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- app/helpers/staffs_helper.rb
+++ app/helpers/staffs_helper.rb
@@ -1,7 +1,7 @@
 module StaffsHelper
   def gravatar_for(staff)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5::hexdigest(staff.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: staff.name, class: "gravatar")
   end
 end
