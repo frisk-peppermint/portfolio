b803aad1263f677f782ad2b2e6894f256cbd29be
atsushi <muraishiatsushiii@gmail.com>
2020-02-05 05:29:48 +0000

    「追加」　ユーザー登録フォームの前まで終わりました。

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,4 +1,8 @@
 class StaffsController < ApplicationController
   def new
   end
+  
+  def show
+    @staff = Staff.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   get    '/stafflogin',   to: 'staffs#new'
   
   resources :users
+  resources :staffs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
