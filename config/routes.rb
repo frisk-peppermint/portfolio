67ede3f9bcb4563fed7a296c54bd4cb61b699bbb
atsushi <muraishiatsushiii@gmail.com>
2020-02-05 04:13:04 +0000

    「追加」　スタッフログインページを作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
 
+
   root 'static_pages#home' # => root_path
-  get    '/contact',   to: 'static_pages#contact'
-  get    '/campaign',  to: 'static_pages#campaign'
-  get    '/signup',    to: 'users#new'
-  get    '/timetable', to: 'users#timetable'
-  post   '/signup',    to: 'users#create'
+  get    '/contact',      to: 'static_pages#contact'
+  get    '/campaign',     to: 'static_pages#campaign'
+  get    '/signup',       to: 'users#new'
+  get    '/timetable',    to: 'users#timetable'
+  post   '/signup',       to: 'users#create'
   
-  get    '/login',     to: 'sessions#new'
-  post   '/login',     to: 'sessions#create'
-  delete '/logout',    to: 'sessions#destroy'
+  get    '/login',        to: 'sessions#new'
+  post   '/login',        to: 'sessions#create'
+  delete '/logout',       to: 'sessions#destroy'
+  
+  get    '/stafflogin',   to: 'staffs#new'
   
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
