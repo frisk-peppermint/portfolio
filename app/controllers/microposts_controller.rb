3a5812934095ad1781e0d3f564dc3b70fc5f3a2a
atsushi <muraishiatsushiii@gmail.com>
2020-02-07 04:24:46 +0000

    マイクロポストを削除できるようになりました。

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
  before_action :logged_in_staff, only: [:create, :destroy]
+ before_action :correct_staff,   only: :destroy
 
   def create
     @micropost = current_staff.microposts.build(micropost_params)
@@ -13,11 +14,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+    
+    def correct_staff
+      @micropost = current_staff.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
