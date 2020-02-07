c8fb2cf418528e6032468198b0a0dddb34eaa738
atsushi <muraishiatsushiii@gmail.com>
2020-02-07 05:07:14 +0000

    画像アップロードにバリデーションを追加しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200207003812) do
+ActiveRecord::Schema.define(version: 20200207044417) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "staff_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["staff_id", "created_at"], name: "index_microposts_on_staff_id_and_created_at"
     t.index ["staff_id"], name: "index_microposts_on_staff_id"
   end
