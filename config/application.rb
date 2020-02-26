88270087fb4c4610483eb8cf161bbfbd16347404
atsushi <muraishiatsushiii@gmail.com>
2020-02-26 14:43:10 +0000

    herokuにデプロイできないエラー対処

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
