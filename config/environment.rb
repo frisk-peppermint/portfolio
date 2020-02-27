ac6326a02486e6596a570d8cb109af3a257d2ae1
atsushi <muraishiatsushiii@gmail.com>
2020-02-27 00:28:32 +0000

    herokuにpushした際にエラーが出たのでconfig/environment.rbのtimeゾーンを削除しました

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 require 'carrierwave/orm/activerecord'
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
