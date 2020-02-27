9d65c699afd4cf39dc9e28d67a55ef4c18249c02
atsushi <muraishiatsushiii@gmail.com>
2020-02-27 16:51:08 +0000

    本日の予約には今日以外の予約が表示されないようにしました。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,25 +8,6 @@ class StaticPagesController < ApplicationController
     
     @timetables = Timetable.all
     
-    @times0900 = Timetable.where(hour: "9", minute: "00") 
-    @times0930 = Timetable.where(hour: "9", minute: "30")
-    @times1000 = Timetable.where(hour: "10", minute: "00")
-    @times1030 = Timetable.where(hour: "10", minute: "30")
-    @times1100 = Timetable.where(hour: "11", minute: "00")
-    @times1130 = Timetable.where(hour: "11", minute: "30")
-    @times1200 = Timetable.where(hour: "12", minute: "00")
-    @times1230 = Timetable.where(hour: "12", minute: "30")
-    @times1500 = Timetable.where(hour: "15", minute: "00")
-    @times1530 = Timetable.where(hour: "15", minute: "30")
-    @times1600 = Timetable.where(hour: "16", minute: "00")
-    @times1630 = Timetable.where(hour: "16", minute: "30")
-    @times1700 = Timetable.where(hour: "17", minute: "00")
-    @times1730 = Timetable.where(hour: "17", minute: "30")
-    @times1800 = Timetable.where(hour: "18", minute: "00")
-    @times1830 = Timetable.where(hour: "18", minute: "30")
-    @times1900 = Timetable.where(hour: "19", minute: "00")
-    @times1930 = Timetable.where(hour: "19", minute: "30")
-    
     @day1 = Date.today
     @day2 = @day1 + 1
     @day3 = @day1 + 2
@@ -35,6 +16,27 @@ class StaticPagesController < ApplicationController
     @day6 = @day1 + 5
     @day7 = @day1 + 6
     @time = Time.now
+    
+    @times0900 = Timetable.where(hour: "9", minute: "00", date: @day1) 
+    @times0930 = Timetable.where(hour: "9", minute: "30", date: @day1)
+    @times1000 = Timetable.where(hour: "10", minute: "00", date: @day1)
+    @times1030 = Timetable.where(hour: "10", minute: "30", date: @day1)
+    @times1100 = Timetable.where(hour: "11", minute: "00", date: @day1)
+    @times1130 = Timetable.where(hour: "11", minute: "30", date: @day1)
+    @times1200 = Timetable.where(hour: "12", minute: "00", date: @day1)
+    @times1230 = Timetable.where(hour: "12", minute: "30", date: @day1)
+    @times1500 = Timetable.where(hour: "15", minute: "00", date: @day1)
+    @times1530 = Timetable.where(hour: "15", minute: "30", date: @day1)
+    @times1600 = Timetable.where(hour: "16", minute: "00", date: @day1)
+    @times1630 = Timetable.where(hour: "16", minute: "30", date: @day1)
+    @times1700 = Timetable.where(hour: "17", minute: "00", date: @day1)
+    @times1730 = Timetable.where(hour: "17", minute: "30", date: @day1)
+    @times1800 = Timetable.where(hour: "18", minute: "00", date: @day1)
+    @times1830 = Timetable.where(hour: "18", minute: "30", date: @day1)
+    @times1900 = Timetable.where(hour: "19", minute: "00", date: @day1)
+    @times1930 = Timetable.where(hour: "19", minute: "30", date: @day1)
+    
+    
   end
   
   def description
--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -6,24 +6,33 @@ class TimetablesController < ApplicationController
   def timetable_index
     @timetables = Timetable.all
     
-    @times0900 = Timetable.where(hour: "9", minute: "00") 
-    @times0930 = Timetable.where(hour: "9", minute: "30")
-    @times1000 = Timetable.where(hour: "10", minute: "00")
-    @times1030 = Timetable.where(hour: "10", minute: "30")
-    @times1100 = Timetable.where(hour: "11", minute: "00")
-    @times1130 = Timetable.where(hour: "11", minute: "30")
-    @times1200 = Timetable.where(hour: "12", minute: "00")
-    @times1230 = Timetable.where(hour: "12", minute: "30")
-    @times1500 = Timetable.where(hour: "15", minute: "00")
-    @times1530 = Timetable.where(hour: "15", minute: "30")
-    @times1600 = Timetable.where(hour: "16", minute: "00")
-    @times1630 = Timetable.where(hour: "16", minute: "30")
-    @times1700 = Timetable.where(hour: "17", minute: "00")
-    @times1730 = Timetable.where(hour: "17", minute: "30")
-    @times1800 = Timetable.where(hour: "18", minute: "00")
-    @times1830 = Timetable.where(hour: "18", minute: "30")
-    @times1900 = Timetable.where(hour: "19", minute: "00")
-    @times1930 = Timetable.where(hour: "19", minute: "30")
+    @day1 = Date.today
+    @day2 = @day1 + 1
+    @day3 = @day1 + 2
+    @day4 = @day1 + 3
+    @day5 = @day1 + 4
+    @day6 = @day1 + 5
+    @day7 = @day1 + 6
+    @time = Time.now
+    
+    @times0900 = Timetable.where(hour: "9", minute: "00", date: @day1) 
+    @times0930 = Timetable.where(hour: "9", minute: "30", date: @day1)
+    @times1000 = Timetable.where(hour: "10", minute: "00", date: @day1)
+    @times1030 = Timetable.where(hour: "10", minute: "30", date: @day1)
+    @times1100 = Timetable.where(hour: "11", minute: "00", date: @day1)
+    @times1130 = Timetable.where(hour: "11", minute: "30", date: @day1)
+    @times1200 = Timetable.where(hour: "12", minute: "00", date: @day1)
+    @times1230 = Timetable.where(hour: "12", minute: "30", date: @day1)
+    @times1500 = Timetable.where(hour: "15", minute: "00", date: @day1)
+    @times1530 = Timetable.where(hour: "15", minute: "30", date: @day1)
+    @times1600 = Timetable.where(hour: "16", minute: "00", date: @day1)
+    @times1630 = Timetable.where(hour: "16", minute: "30", date: @day1)
+    @times1700 = Timetable.where(hour: "17", minute: "00", date: @day1)
+    @times1730 = Timetable.where(hour: "17", minute: "30", date: @day1)
+    @times1800 = Timetable.where(hour: "18", minute: "00", date: @day1)
+    @times1830 = Timetable.where(hour: "18", minute: "30", date: @day1)
+    @times1900 = Timetable.where(hour: "19", minute: "00", date: @day1)
+    @times1930 = Timetable.where(hour: "19", minute: "30", date: @day1)
   end
   
   def new
@@ -38,10 +47,10 @@ class TimetablesController < ApplicationController
     
     if (@reservations.count < 4) && @user_id_in_timetable.empty?
       @timetable.save
-      flash[:success] = "ご予約ありがとうございます。#{@timetable.hour}時#{@timetable.minute}分にお待ちしております"
+      flash[:success] = "ご予約ありがとうございます。#{@timetable.date}日#{@timetable.hour}時#{@timetable.minute}分にお待ちしております"
       redirect_to root_path
     else
-      flash[:success] = "申し訳ございません。すでに予約が上限に達しているため、受け付けられませんでした。"
+      flash[:danger] = "申し訳ございません。すでに予約が上限に達しているため、受け付けられませんでした。"
       redirect_to root_path
     end
     
